cca5b7852be2d3a05c351d34bda07e3d343099d7
nitish-creat <nitishrana2242006@gmail.com>
2026-06-17 22:04:48 +0530

    stack practise

---
+++ STACK/QueueUsingStack.cpp
@@ -0,0 +1,14 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class MyQueue{
+//     stack<int> st;
+//     void enqueue(int x){
+//         int 
+//     }
+// };
+
+// int main() {
+    
+//     return 0;
+// }
